5a1e2bbe8df2ea960dc4e0d782568a4d8195e57b
owen <wentaiouyang@gmail.com>
2025-11-26 16:40:28 +0800

    fix: update interface

--- js/script_welcome.js
+++ js/script_welcome.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+    $(".arrow").on("click", function () {
+        const target = $("#about");
+        if (!target.length) {
+            return false;
+        }
 
-    $(".arrow").click(function () {
-        $('html, body').animate({scrollTop:$(document).height()},1500);
+        $("html, body").animate(
+            { scrollTop: target.offset().top - 24 },
+            900
+        );
         return false;
     });
 });
-
-
-
